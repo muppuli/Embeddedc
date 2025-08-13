ac5c61a860821a955249d4530386fb73d5362b8a
Muppuli <muppulim699@gmail.com>
2025-08-13 18:33:13 +0530

    code4

---
+++ c8.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("KGISL INSTITUTE OF TECHNOLOGY");
+    return 0;
+}
---
+++ c9.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("Welcome to C\n");
+    printf("Let's start coding");
+    return 0;
+}
